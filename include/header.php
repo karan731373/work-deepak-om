797bbe7271092f5c598fa80ee0a55b286ae59cbe
shikhartpl <karan731373@gmail.com>
2026-03-11 13:29:37 +0530

    changes done

---
+++ include/header.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
+
+    <title>Om Exports</title>
+
+    <!-- Stylesheets -->
+    <link href="css/style.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+
+</head>
+
+<!-- page wrapper -->
+
+<body class="boxed_wrapper">
+
+
+    <!-- preloader -->
+    <div class="preloader"></div>
+    <!-- preloader -->
+
+
+
+
+    <!-- main header -->
+    <header class="main-header style-two">
+        <!-- header-top -->
+        <!-- <div class="header-top">
+            <div class="container">
+                <div class="inner-container clearfix">
+                    <ul class="header-info pull-left">
+                        <li>Time : Monday-Friday 09am-5pm</li>
+                        <li><i class="flaticon-telephone-auricular-with-cable"></i><a href="tel:(+88)01912704287">(+88) 01912704287</a></li>
+                    </ul>
+                    <ul class="header-nav pull-right">
+                        <li><a href="#">Careers</a></li>
+                        <li><a href="#">Contacts</a></li>
+                        <li>
+                            <div class="language dropdownn">
+                                <a class="dropdown-toggle" id="dropdownMenu4" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true" href="#">Eng</a>
+                                <ul class="dropdown-menu" aria-labelledby="dropdownMenu4">
+                                    <li><a href="#">Eng</a></li>
+                                    <li><a href="#">Chi</a></li>
+                                    <li><a href="#">Ger</a></li>
+                                    <li><a href="#">Fre</a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div> -->
+
+        <!-- header-bottom -->
+        <div class="header-bottom">
+            <div class="container">
+                <div class="outer-container">
+                    <div class="nav-outer clearfix">
+                        <div class="menu-area pull-left clearfix">
+                            <figure class="logo-box"><a href="index.html"><img src="images/om-logo.png" alt=""></a>
+                            </figure>
+                            <nav class="main-menu navbar-expand-lg">
+                                <div class="navbar-header">
+                                    <!-- Toggle Button -->
+                                    <button type="button" class="navbar-toggle" data-toggle="collapse"
+                                        data-target=".navbar-collapse">
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                        <span class="icon-bar"></span>
+                                    </button>
+                                </div>
+                                <div class="navbar-collapse collapse clearfix">
+                                    <ul class="navigation clearfix">
+
+                                        <li class=""><a href="index-2.html">Home</a></li>
+                                        <li class=""><a href="about.html">About Us</a></li>
+                                        <li class="current dropdown"><a href="#">Products</a>
+                                            <ul>
+                                                <li><a href="product.html#form">Form Works</a></li>
+                                                <li><a href="product.html#scaffoldings">Scaffoldings</a></li>
+                                                <li><a href="product.html#scaffoldings">Geotechnics</a></li>
+                                                <li><a href="product.html#fabrication">Fabrication</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class=""><a href="infrastructure.html">Infrastructure</a></li>
+                                        <li class=""><a href="quality.html">Quality</a></li>
+                                        <li class=""><a href="contact.html">Contact us</a></li>
+
+                                    </ul>
+                                </div>
+                            </nav>
+                        </div>
+                        <div class="info-box pull-right clearfix">
+
+                            <div class="btn-box"><a href="#"><i class="fas fa-arrow-right"></i>Catalogue</a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- header-bottom end -->
+
+        <!--sticky Header-->
+        <!-- <div class="sticky-header">
+            <div class="container clearfix">
+                <figure class="logo-box"><a href="index.html"><img src="images/small-logo.png" alt=""></a></figure>
+                <div class="menu-area">
+                    <nav class="main-menu navbar-expand-lg">
+                        <div class="navbar-header">
+                               
+                            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            </button>
+                        </div>
+                        <div class="navbar-collapse collapse clearfix">
+                            <ul class="navigation clearfix">
+                                <li class="current dropdown"><a href="#">Home</a>
+                                    <ul>
+                                        <li><a href="index.html">Home Factory</a></li>
+                                        <li><a href="index-2.html">Home Industry</a></li>
+                                        <li><a href="index-3.html">Home Construction</a></li>
+                                        <li><a href="index-4.html">Home Oil & Gas</a></li>
+                                        <li><a href="index-5.html">Home Power Energy</a></li>
+                                        <li><a href="index-6.html">Home Industrial Machinery</a></li>
+                                        <li><a href="index-7.html">Home Fabric & Textile</a></li>
+                                        <li><a href="index-8.html">Home Medical Equepments</a></li>
+                                        <li class="dropdown"><a href="#">Header Styles</a>
+                                            <ul>
+                                                <li><a href="index.html">Header Style 01</a></li>
+                                                <li><a href="index-2.html">Header Style 02</a></li>
+                                                <li><a href="index-3.html">Header Style 03</a></li>
+                                                <li><a href="index-4.html">Header Style 04</a></li>
+                                                <li><a href="index-5.html">Header Style 05</a></li>
+                                                <li><a href="index-6.html">Header Style 06</a></li>
+                                                <li><a href="index-7.html">Header Style 07</a></li>
+                                                <li><a href="index-8.html">Header Style 08</a></li>
+                                            </ul>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown"><a href="#">Page</a>
+                                    <ul>
+                                        <li class="dropdown"><a href="#">About Page</a>
+                                            <ul>
+                                                <li><a href="about.html">About Type 01</a></li>
+                                                <li><a href="about-2.html">About Type 02</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="dropdown"><a href="#">Team Page</a>
+                                            <ul>
+                                                <li><a href="team.html">Team Type 01</a></li>
+                                                <li><a href="team-2.html">Team Type 02</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="dropdown"><a href="#">Service Page</a>
+                                            <ul>
+                                                <li><a href="service.html">Service Type 01</a></li>
+                                                <li><a href="service-2.html">Service Type 02</a></li>
+                                                <li><a href="service-3.html">Service Type 03</a></li>
+                                                <li><a href="service-single.html">Service Single 01</a></li>
+                                                <li><a href="service-single-2.html">Service Single 02</a></li>
+                                            </ul>
+                                        </li>
+                                        <li class="dropdown"><a href="#">Shop Page</a>
+                                            <ul>
+                                                <li><a href="shop.html">Shop Type 01</a></li>
+                                                <li><a href="shop-2.html">Shop Type 02</a></li>
+                                                <li><a href="shop-single.html">Single Shop</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="history.html">Our History</a></li>
+                                        <li><a href="partner.html">Our Partners</a></li>
+                                        <li><a href="testimonial.html">Our Testimonial</a></li>
+                                        <li><a href="gallery.html">Our Gallery</a></li>
+                                        <li><a href="coming-soon.html">Coming Soon</a></li>
+                                        <li><a href="error.html">Error Page</a></li>
+                                    </ul>
+                                </li> 
+                                <li class="dropdown"><a href="#">Case Studies</a>
+                                    <ul>
+                                        <li><a href="case.html">Case Type 01</a></li>
+                                        <li><a href="case-2.html">Case Type 02</a></li>
+                                        <li><a href="case-3.html">Case Type 03</a></li>
+                                        <li><a href="case-4.html">Case Type 04</a></li>
+                                        <li><a href="case-single.html">Case Single 01</a></li>
+                                        <li><a href="case-single-2.html">Case Single 02</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown"><a href="#">Blog</a>
+                                    <ul>
+                                        <li><a href="blog-grid.html">Blog Grid</a></li>
+                                        <li><a href="blog-classic.html">Blog Classic</a></li>
+                                        <li><a href="blog-single.html">Blog Single</a></li>
+                                    </ul>
+                                </li>                              
+                                <li class="dropdown"><a href="#">Contact</a>
+                                    <ul>
+                                        <li><a href="contact.html">Contact Type 01</a></li>
+                                        <li><a href="contact-2.html">Contact Type 02</a></li>
+                                    </ul>
+                                </li> 
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div> -->
+
+    </header>
+    <!-- main-header end -->
